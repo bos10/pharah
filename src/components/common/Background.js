2aa951014ae76e7e89914d21135304ccebfa4e82
Bos <bostoncheong@gmail.com>
2018-07-01 15:52:29 +0800

    Added linear gradient successfully

--- src/components/common/Background.js
+++ src/components/common/Background.js
@@ -1,22 +1,30 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 
 // Styling for BACKGROUND (Wallpaper)
 const Background = (props) => {
   return (
-    <View style={styles.containerStyle}>
-      {props.children}
-    </View>
+    // Top, Mid, Bottom (Gradient)
+    <LinearGradient colors={['#FFAF54', '#ff9e54', '#D17845']} style={styles.linearGradient}>
+        {props.children}
+    </LinearGradient>
   );
 };
 
 const styles = {
-  containerStyle: {
+  // containerStyle: {
+  //   flex: 1,
+  //   width: null,
+  //   height: null,
+  //   backgroundColor: '#FF0000',
+  // },
+  linearGradient: {
     flex: 1,
-    width: null,
-    height: null,
-    backgroundColor: '#FF0000',
-  }
+    paddingLeft: 15,
+    paddingRight: 15,
+    borderRadius: 5
+  },
 };
 
 export { Background };
