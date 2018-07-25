f3e97d5e6160b8d423672cc3eca8a0c14bec8996
ihwk1996 <ihwk1996@gmail.com>
2018-07-25 21:48:55 +0800

    - finish up boston bugs

--- src/components/FoodListItem.js
+++ src/components/FoodListItem.js
@@ -44,61 +44,61 @@ class FoodListItem extends Component {
       switch (key) {
         // THAI KITCHEN
         // Fried Rice
-        case 'T51-ChineseStyle': totalCost += value * 5.50; break;
-        case 'T52-ThaiStyle': totalCost += value * 5.50; break;
-        case 'T53-Ikan Bilis(Anchovies)': totalCost += value * 4.50; break;
-        case 'T54-Tomato&Chicken': totalCost += value * 5.50; break;
+        case 'T51-ChineseStyle': cost = 5.50; totalCost += value * 5.50; break;
+        case 'T52-ThaiStyle': cost = 5.50; totalCost += value * 5.50; break;
+        case 'T53-Ikan Bilis(Anchovies)': cost = 4.50; totalCost += value * 4.50; break;
+        case 'T54-Tomato&Chicken': cost = 5.50; totalCost += value * 5.50; break;
 
         // Noodles
-        case 'T35-Tomyam (Soup)': totalCost += value * 6.50; break;
-        case 'T36-Pataya (Egg Wrap)': totalCost += value * 6.50; break;
-        case 'T37-Soup with Vegetables': totalCost += value * 5.50; break;
-        case 'T38-Bandung (Red Sauce)': totalCost += value * 5.50; break;
-        case 'T41-Thai Style Fried': totalCost += value * 5.50; break;
+        case 'T35-Tomyam (Soup)': cost = 6.50; totalCost += value * 6.50; break;
+        case 'T36-Pataya (Egg Wrap)': cost = 6.50; totalCost += value * 6.50; break;
+        case 'T37-Soup with Vegetables': cost = 5.50; totalCost += value * 5.50; break;
+        case 'T38-Bandung (Red Sauce)': cost = 5.50; totalCost += value * 5.50; break;
+        case 'T41-Thai Style Fried': cost = 5.50; totalCost += value * 5.50; break;
 
         // Steam Rice
-        case 'T45-Hot & Spicy': totalCost += value * 6.00; break;
-        case 'T46-Black Soya Sauce': totalCost += value * 6.00; break;
-        case 'T47-Sweet & Sour': totalCost += value * 6.00; break;
-        case 'T48-Black Pepper': totalCost += value * 6.00; break;
+        case 'T45-Hot & Spicy': cost = 6.00; totalCost += value * 6.00; break;
+        case 'T46-Black Soya Sauce': cost = 6.00; totalCost += value * 6.00; break;
+        case 'T47-Sweet & Sour': cost = 6.00; totalCost += value * 6.00; break;
+        case 'T48-Black Pepper': cost = 6.00; totalCost += value * 6.00; break;
 
         // WESTERN KITCHEN
         // Chicken
-        case 'W40-Grilled Chicken with Pepper': totalCost += value * 11.8; break;
+        case 'W40-Grilled Chicken with Pepper': cost = 11.80; totalCost += value * 11.80; break;
         case 'W41-Grilled Chicken with Mushroom': totalCost += value * 13.80; break;
-        case 'W42-Breaded Cutlet': totalCost += value * 10.80; break;
+        case 'W42-Breaded Cutlet': cost = 10.80; totalCost += value * 10.80; break;
 
         // Lamb
-        case 'W43-Grilled Lamb with Pepper': totalCost += value * 20.50; break;
-        case 'W44-Grilled Lamb with Mushroom': totalCost += value * 21.50; break;
-        case 'W45-BarBeQue Lamb with Cheese Sauce': totalCost += value * 21.80; break;
+        case 'W43-Grilled Lamb with Pepper': cost = 20.50; totalCost += value * 20.50; break;
+        case 'W44-Grilled Lamb with Mushroom': cost = 21.50; totalCost += value * 21.50; break;
+        case 'W45-BarBeQue Lamb with Cheese Sauce': cost = 21.80; totalCost += value * 21.80; break;
 
         // INDIAN KITCHEN
         // Chicken
-        case 'N63-Chicken Korma': totalCost += value * 7; break;
-        case 'N64-Chicken Spinach': totalCost += value * 8; break;
-        case 'N65-Chicken Masala': totalCost += value * 7; break;
+        case 'N63-Chicken Korma': cost = 7; totalCost += value * 7; break;
+        case 'N64-Chicken Spinach': cost = 8; totalCost += value * 8; break;
+        case 'N65-Chicken Masala': cost = 7; totalCost += value * 7; break;
         // Mutton
-        case 'N79-Mutton Korma': totalCost += value * 8; break;
-        case 'N80-Mutton Masala': totalCost += value * 8; break;
-        case 'N81-Mutton Do Piaza': totalCost += value * 9.00; break;
+        case 'N79-Mutton Korma': cost = 8; totalCost += value * 8; break;
+        case 'N80-Mutton Masala': cost = 8; totalCost += value * 8; break;
+        case 'N81-Mutton Do Piaza': cost = 9; totalCost += value * 9; break;
 
         // DRINKS
         // Hot
-        case 'D58-Tea': totalCost += value * 1.5; break;
-        case 'D59-Coffee': totalCost += value * 1.5; break;
-        case 'D60-Nescafe': totalCost += value * 1.8; break;
+        case 'D58-Tea': cost = 1.5; totalCost += value * 1.5; break;
+        case 'D59-Coffee': cost = 1.5; totalCost += value * 1.5; break;
+        case 'D60-Nescafe': cost = 1.8; totalCost += value * 1.8; break;
         // Cold
-        case 'D74-Ice Tea': totalCost += value * 2; break;
-        case 'D75-Ice Coffee': totalCost += value * 2; break;
-        case 'D76-Ice Nescafe': totalCost += value * 2.5; break;
+        case 'D74-Ice Tea': cost = 2; totalCost += value * 2; break;
+        case 'D75-Ice Coffee': cost = 2; totalCost += value * 2; break;
+        case 'D76-Ice Nescafe': cost = 2.5; totalCost += value * 2.5; break;
 
         // DESERT
         // Ice Cream
         case 'D90-Dark Lava Cake with Strawberry and Milk Ice Cream':
-              totalCost += value * 6.8; break;
-        case 'D91-Mix Berry Cheese Cake': totalCost += value * 7.5; break;
-        case 'D94-Banana Split': totalCost += value * 4.8; break;
+              cost = 6.8; totalCost += value * 6.8; break;
+        case 'D91-Mix Berry Cheese Cake': cost = 7.5; totalCost += value * 7.5; break;
+        case 'D94-Banana Split': cost = 4.8; totalCost += value * 4.8; break;
 
         default: cost = 0;
       }
